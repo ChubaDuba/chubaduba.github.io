5177d2d80e177a37bbe4ade3e32343d85e5ff30d
chubaduba <achubakov@gmail.com>
2018-03-04 12:40:30 +0300

    edit

---
+++ files/async.js
@@ -0,0 +1,8 @@
+<code>
+$(document).ready(function() {
+    var script = document.createElement("script");
+    script.src = "dummy_js.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+);
+</code>
